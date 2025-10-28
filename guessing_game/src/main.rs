9bbf9ef6f63a8505f57a0fa0cc28dbfa7cbc5cd7
Lviffy <contact.rohan.here@gmail.com>
2025-10-29 03:58:28 +0530

    Guess game in RUST

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,13 +1,35 @@
 use std::io;
+use rand::{Rng, rng};
+use std::cmp::Ordering;
 
 fn main(){
-    println!("Guess the Number!");
-    println!("Enter your Guess?");
 
-    let mut guess = String::new();
+    println!("Guess the Number!");
+    let secret_number = rng().random_range(1..=100);
+    println!("Secret Number = {secret_number}");
+loop{
+
+    println!("Enter your Guess?");
+    let mut guess = String::new(); //append make sure inside loop
 
     io::stdin().read_line(&mut guess)
         .expect("could not read input");
 
-    println!("Your guess is {guess}");
+    let guess : u32 =match guess.trim().parse(){
+        Ok(num) => num,
+        Err(_) => {
+            println!("please enter a valid input");
+            continue;
+        }
+    }; //shadowing previous variable after parsing
+    
+   match guess.cmp(&secret_number){     //all possible cases have to cover in match
+        Ordering::Equal => {
+            println!("you won!");
+            break;
+        },
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Less => println!("Too small!")
+   }
+} 
 }
